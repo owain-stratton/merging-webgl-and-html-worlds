4abac02fb430b21c374a30f89ea5d8eecbaf2026
Owain Stratton <owain@corduroystudio.io>
2022-01-14 14:40:34 +0100

    chore: working with shaders and threejs

---
+++ js/app.js
@@ -0,0 +1,69 @@
+import * as THREE from 'three';
+import { OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+import fragment from './shaders/fragment.glsl'
+import vertex from './shaders/vertex.glsl'
+
+export default class Sketch {
+  constructor(options) {
+    this.time = 0
+    this.container = options.dom
+    this.scene = new THREE.Scene();
+
+    this.width = this.container.offsetWidth
+    this.height = this.container.offsetHeight
+    this.camera = new THREE.PerspectiveCamera( 70, this.width / this.height, 0.01, 10 );
+    this.camera.position.z = 1;
+
+    this.renderer = new THREE.WebGLRenderer( { antialias: true } );
+    this.renderer.setSize( this.width, this.height );
+    this.container.appendChild(this.renderer.domElement)
+
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+
+    this.resize()
+    this.setupResize()
+    this.addObjects()
+    this.render()
+}
+
+  addObjects() {
+    this.geometry = new THREE.PlaneBufferGeometry( 4, 4, 100, 100 );
+    this.material = new THREE.MeshNormalMaterial();
+
+    this.material = new THREE.ShaderMaterial({ 
+      uniforms: {time: {value: 0}},
+      side: THREE.DoubleSide,
+      fragmentShader:fragment, 
+      vertexShader: vertex,
+      // wireframe: true
+    })
+
+    this.mesh = new THREE.Mesh( this.geometry, this.material );
+    this.scene.add( this.mesh );
+  }
+
+  setupResize() {
+    window.addEventListener('resize', this.resize.bind(this))
+  }
+
+  resize() {
+    this.width = this.container.offsetWidth
+    this.height = this.container.offsetHeight
+    this.renderer.setSize(this.width, this.height);
+    this.camera.aspect = this.width / this.height
+    this.camera.updateProjectionMatrix()
+  }
+
+  render() {
+    this.time+=0.05
+    this.mesh.rotation.x = this.time / 2000;
+	  this.mesh.rotation.y = this.time / 1000;
+	  this.renderer.render( this.scene, this.camera );
+    this.material.uniforms.time.value = this.time;
+    window.requestAnimationFrame(this.render.bind(this))
+  }
+} 
+
+new Sketch({
+  dom: document.getElementById('container')
+})
