4b9a2376509174e1077932a5278a9f30c337e3d3
Owain Stratton <owain@corduroystudio.io>
2022-01-14 18:36:06 +0100

    chore: next section of the course

--- js/app.js
+++ js/app.js
@@ -1,34 +1,62 @@
 import * as THREE from 'three';
+import imagesLoaded from 'imagesloaded'
+import FontFaceObserver from 'fontfaceobserver';
 import { OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 import fragment from './shaders/fragment.glsl'
 import vertex from './shaders/vertex.glsl'
 import ocean from '../img/oceans.jpg'
 export default class Sketch {
-  constructor(options) {
+   constructor(options) {
     this.time = 0
     this.container = options.dom
     this.scene = new THREE.Scene();
 
     this.width = this.container.offsetWidth
     this.height = this.container.offsetHeight
-    this.camera = new THREE.PerspectiveCamera( 70, this.width / this.height, 0.01, 10 );
-    this.camera.position.z = 1;
+    this.camera = new THREE.PerspectiveCamera( 70, this.width / this.height, 100, 2000 );
+    this.camera.position.z = 600;
 
-    this.renderer = new THREE.WebGLRenderer( { antialias: true } );
+    this.camera.fov = 2*Math.atan((this.height/2)/600)*(180/Math.PI);
+
+    this.renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true } );
     this.renderer.setSize( this.width, this.height );
     this.container.appendChild(this.renderer.domElement)
 
     this.controls = new OrbitControls(this.camera, this.renderer.domElement)
 
-    this.resize()
-    this.setupResize()
-    this.addObjects()
-    this.render()
+    this.images = [...document.querySelectorAll('img')]
+    
+    const fontOpen = new Promise(resolve => {
+      new FontFaceObserver("Open Sans").load().then(() => {
+        resolve();
+      });
+    });
+
+    const fontPlayfair = new Promise(resolve => {
+      new FontFaceObserver("Playfair Display").load().then(() => {
+        resolve();
+      });
+    });
+
+    // Preload images
+    const preloadImages = new Promise((resolve, reject) => {
+        imagesLoaded(document.querySelectorAll("img"), { background: true }, resolve);
+    });
+
+     Promise.all([fontOpen, fontPlayfair, preloadImages]).then(() => {
+       this.addImages()
+       this.setPosition()
+       this.resize()
+       this.setupResize()
+       this.addObjects()
+       this.render()
+     })
+
 }
 
   addObjects() {
-    this.geometry = new THREE.PlaneBufferGeometry( 1, 1, 40, 40 );
-    this.geometry = new THREE.SphereBufferGeometry( 0.4, 40, 40 );
+    this.geometry = new THREE.PlaneBufferGeometry( 100, 100, 10, 10 );
+    // this.geometry = new THREE.SphereBufferGeometry( 0.4, 40, 40 );
     this.material = new THREE.MeshNormalMaterial();
 
     this.material = new THREE.ShaderMaterial({ 
@@ -48,6 +76,38 @@ export default class Sketch {
 
   setupResize() {
     window.addEventListener('resize', this.resize.bind(this))
+  }
+
+  addImages() {
+    this.imageStore = this.images.map(img => {
+      let bounds = img.getBoundingClientRect();
+
+      let geometry = new THREE.PlaneBufferGeometry(bounds.width, bounds.height, 1,1);
+      let texture = new THREE.Texture(img);
+      texture.needsUpdate = true
+      let material = new THREE.MeshBasicMaterial({ 
+        // color: 0xff0000, 
+        map: texture 
+      });
+      let mesh = new THREE.Mesh(geometry, material);
+      this.scene.add(mesh)
+
+      return {
+        img,
+        mesh,
+        top: bounds.top,
+        left: bounds.left,
+        width: bounds.width, 
+        height: bounds.height
+      }
+    })
+  }
+
+  setPosition() {
+    this.imageStore.forEach(o => {
+      o.mesh.position.y = -o.top + this.height/2 - o.height/2;
+      o.mesh.position.x = o.left - this.width/2 + o.width/2;
+    })
   }
 
   resize() {
